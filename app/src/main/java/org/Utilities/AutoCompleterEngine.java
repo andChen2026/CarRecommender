290767c5bf286600c97e3c1d1b2df77667c59d18
ItsAndrewC! <andew73885@gmail.com>
2024-08-07 16:09:26 -0400

    finishing layout

--- app/src/main/java/org/Utilities/AutoCompleterEngine.java
+++ app/src/main/java/org/Utilities/AutoCompleterEngine.java
@@ -1,5 +1,9 @@
 package org.Utilities;
 
+/*
+ A Naive Autocompleter Engine - Uses a DLB as the backing data structure
+ @author Andrew Chen
+ */
 public class AutoCompleterEngine {
     
 }
--- app/src/main/java/org/Utilities/AutoCorrectorEngine.java
+++ app/src/main/java/org/Utilities/AutoCorrectorEngine.java
@@ -1,5 +1,9 @@
 package org.Utilities;
 
+/*
+ A Naive AutoCorrector Engine - Uses a DLB as the backing data structure
+ @author Andrew Chen
+ */
 public class AutoCorrectorEngine {
     
 }
